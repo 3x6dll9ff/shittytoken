2b1903cc5793f1d42c5a64a1524ab601064bb868
sklDanila <danila.sklovskii@gmail.com>
2024-07-27 16:34:52 +0200

    search-bar

--- src/quests.js
+++ src/quests.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import './css/quests.css'
+import searchIcon from './assets/images/searchIcon.png'
+import './assets/fonts/pixel_operator/PixelOperator.ttf'
 
 
 class Quests extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        };
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ query: event.target.value });
+    };
+
+    handleSearch = () => {
+        console.log('Search query:', this.state.query);
+        // Логика обработки поиска
+    };
+
     render() {
         return (
             <div>
-                Quests
+                <div className="search-bar-area">
+                    <div className='search-bar'>
+                        <div className='frame-search-bar'>
+                            <img src={searchIcon} alt="Search Icon" />
+                        </div>
+                        <input
+                            type="text"
+                            value={this.state.query}
+                            onChange={this.handleInputChange}
+                            placeholder="Search"
+                            style={{  backgroundColor: 'black', fontFamily: 'Pixel Operator HB", sans-serif' }}
+                        />
+                    </div>
+                </div>
+                <div className='welcome-banner'>
+                    <div className='welcome-banner-text'>
+                        <h1>Welcome to the Quests page!</h1>
+                    </div>
+                </div>
             </div>
         );
     }
