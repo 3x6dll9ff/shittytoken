ab7ffa995a510552020884d1b0f5be2060388248
Elisei <10thecrew01@gmail.com>
2024-07-30 18:55:03 +0200

    card description on stuff page

--- src/components/stuff/card.js
+++ src/components/stuff/card.js
@@ -1,18 +1,54 @@
-// src/components/BackgroundVideo.js
 import React from 'react';
 import '../../css/stuff.css';
 
-const Card = ({name, image}) => {
+const Card = ({ signature, image, description_data }) => {
+  const { name, aka, age, location, role, nationality, balance, crimes } = description_data;
+
   return (
     <div className="card-container">
-        <div className='stuff-card'>
-            <div className='card-image-container'>
-              <img className="card-image" src={image} alt="CardImage" />
-            </div>
-            <div className='card-name'>
-                <p>{name}</p>
-            </div>
+      <div className='stuff-card'>
+        <div className='card-image-container'>
+          <img className="card-image" src={image} alt="CardImage" />
         </div>
+        <div className='card-signature'>
+          <p>{signature}</p>
+        </div>
+        <div className='card-description'>
+          <div className='small-img'>
+            <img src={image} alt="CardImageSmall" />
+          </div>
+          <div className='card-id-container'>
+            <p>ID CARD</p>
+          </div>
+          <p className='card-name'>
+            Name: <span className='card-green-text'>{name}</span>
+          </p>
+          <p className='card-aka'>
+            Aka: <span className='card-green-text'>{aka}</span>
+          </p>
+          <p className='card-age'>
+            Age: <span className='card-green-text'>{age}</span>
+          </p>
+          <p className='card-location'>
+            Location: <span className='card-green-text'>{location}</span>
+          </p>
+          <p className='card-role'>
+            Role: <span className='card-green-text'>{role}</span>
+          </p>
+          <p className='card-nationality'>
+            Nationality: <span className='card-green-text'>{nationality}</span>
+          </p>
+          <p className='card-balance'>
+            Estimated balance: <span className='card-green-text'>{balance}</span>
+          </p>
+          <div className='card-crimes'>
+            <p>CYBER VIOLATIONS</p>
+            <ul>
+              {crimes.map((crime, index) => <li key={index}>{crime}</li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/stuff/stuff.js
+++ src/components/stuff/stuff.js
@@ -8,14 +8,32 @@ import hacker1 from '../../assets/images/hackers/hacker1.png'
 import hacker2 from '../../assets/images/hackers/hacker2.png'
 import hacker3 from '../../assets/images/hackers/hacker3.png'
 
+
+const cardData = {
+  name: "Name Example",
+  aka: "Aka Example",
+  age: 30,
+  location: "Location",
+  role: "Architect",
+  nationality: "Nationality",
+  balance: "100 BTC",
+  crimes: [
+    "SQL-injections",
+    "Hacking corporate networks",
+    "Phishing",
+    "DDoS attacks"
+  ]
+};
+
+
 const Stuff = () => {
   return (
     <div className="stuff-container">
       <PageName pageName='STUFF'/>
-      <Card name='@HAZE' image={hacker1}/>
-      <Card name='@NOTHACKER' image={hacker2}/>
-      <Card name='@FINGERPRINT' image={hacker3}/>
-      <Card name='@HACKER4' image={hacker2}/>
+      <Card signature='@HAZE' image={hacker1} description_data={cardData}/>
+      <Card signature='@NOTHACKER' image={hacker2} description_data={cardData}/>
+      <Card signature='@FINGERPRINT' image={hacker3} description_data={cardData}/>
+      <Card signature='@HACKER4' image={hacker2} description_data={cardData}/>
     </div>
   );
 };
