d7fda1e999adb61954940d17eca83ffaaaaacccb
PatchedDev <danila.kardashevskii@icloud.com>
2024-08-19 13:05:15 +0200

    fix stuff card

--- src/home/components/stuff_slide.jsx
+++ src/home/components/stuff_slide.jsx
@@ -3,24 +3,24 @@ import SlideTitle from './slide_title.jsx';
 import Stuff_slide_card from './stuff_slide_card.jsx';
 import '../css/stuff.css';
 
-import hacker1 from '../assets/images/hackers/hacker1.png';
+import hacker1 from '../assets/images/hackers/hacker1.jpg';
 import hacker2 from '../assets/images/hackers/hacker2.png';
 import hacker3 from '../assets/images/hackers/hacker3.png';
 
 
-const cardData = {
-    name: "Name Example",
-    aka: "Aka Example",
+const cardData_hacker1 = {
+    name: "Dylan Campbell",
+    aka: "Haze",
+    nationality: "Canadian",
+    location: "Toronto",
+    balance: "17 BTC",
     age: 30,
-    location: "Location",
     role: "Architect",
-    nationality: "Nationality",
-    balance: "100 BTC",
     crimes: [
-        "SQL-injections",
-        "Hacking corporate networks",
+        "Cryptojacking",
+        "ICO scams",
+        "Smart contract exploits",
         "Phishing",
-        "DDoS attacks"
     ]
 };
 
@@ -29,10 +29,10 @@ const Stuff_slide = () => {
     return (
         <div className="stuff-container">
             <SlideTitle pageName='STUFF'/>
-            <Stuff_slide_card signature='@HAZE' image={hacker1} description_data={cardData}/>
-            <Stuff_slide_card signature='@NOTHACKER' image={hacker2} description_data={cardData}/>
-            <Stuff_slide_card signature='@FINGERPRINT' image={hacker3} description_data={cardData}/>
-            <Stuff_slide_card signature='@HACKER4' image={hacker2} description_data={cardData}/>
+            <Stuff_slide_card signature='@HAZE' image={hacker1} description_data={cardData_hacker1}/>
+            <Stuff_slide_card signature='@NOTHACKER' image={hacker2} description_data={cardData_hacker1}/>
+            <Stuff_slide_card signature='@FINGERPRINT' image={hacker3} description_data={cardData_hacker1}/>
+            <Stuff_slide_card signature='@HACKER4' image={hacker2} description_data={cardData_hacker1}/>
         </div>
     );
 };
