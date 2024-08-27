3d4c29d9beffa38aa84f1e042976174031b35d23
LooNuH <mail.loonuh@gmail.com>
2024-08-27 16:12:36 +0200

    moved connect-wallet-button css properties to its own file

--- src/global/components/connect-wallet-button/component.jsx
+++ src/global/components/connect-wallet-button/component.jsx
@@ -1,5 +1,7 @@
 import {connectWallet, okxReplaces, walletInstalled} from "../../scripts/wallets_connector.js";
 
+import "./css/connect-wallet-button.css";
+
 import metamask_icon from "../../assets/images/metamask-logo.png";
 import rabby_icon from "../../assets/images/rabby-logo.png";
 import phantom_icon from "../../assets/images/phantom-logo.png";
@@ -25,31 +27,31 @@ const ConnectWalletButton = ({wallet, connectType, onWalletConnect}) => {
 
     return (
         <div
-            className={`header-data-popup-connect-wallet-button ${walletInstalled(wallet) ? '' : 'disabled'}`}
+            className={`connect-wallet-button ${walletInstalled(wallet) ? '' : 'disabled'}`}
             onClick={walletInstalled(wallet) ? () => connectWallet(wallet, connectType, onWalletConnect) : null}
         >
-            <div className={`header-data-popup-connect-wallet-button-icon-container`}>
+            <div className={`connect-wallet-button-icon-container`}>
                 {okxReplaces(wallet) ? (
                     <img
-                        className={`header-data-popup-connect-wallet-button-icon right ${walletInstalled(wallet) ? '' : 'disabled'}`}
+                        className={`connect-wallet-button-icon right ${walletInstalled(wallet) ? '' : 'disabled'}`}
                         src={okx_icon}
                         alt={`header-profile-wallet-popup-button-icon`}
                     />
                 ) : null}
                 <img
-                    className={`header-data-popup-connect-wallet-button-icon ${okxReplaces(wallet) ? 'left' : ''} ${walletInstalled(wallet) ? '' : 'disabled'}`}
+                    className={`connect-wallet-button-icon ${okxReplaces(wallet) ? 'left' : ''} ${walletInstalled(wallet) ? '' : 'disabled'}`}
                     src={walletIcons[wallet]}
                     alt={`header-profile-wallet-popup-button-icon`}
                 />
             </div>
             <div
-                className={`header-data-popup-connect-wallet-button-wallet-name-container ${walletInstalled(wallet) ? '' : 'disabled'}`}>
+                className={`connect-wallet-button-wallet-name-container ${walletInstalled(wallet) ? '' : 'disabled'}`}>
                 <div
-                    className={`header-data-popup-connect-wallet-button-wallet-name ${walletInstalled(wallet) ? '' : 'disabled'}`}>
+                    className={`connect-wallet-button-wallet-name ${walletInstalled(wallet) ? '' : 'disabled'}`}>
                     {walletNames[wallet] || wallet} {okxReplaces(wallet) ? '/ OKX Wallet' : ''}
                 </div>
                 <div
-                    className={`header-data-popup-connect-wallet-button-wallet-status ${walletInstalled(wallet) ? 'green' : 'red'}`}>
+                    className={`connect-wallet-button-wallet-status ${walletInstalled(wallet) ? 'green' : 'red'}`}>
                     {walletInstalled(wallet) ? 'Installed' : 'Not installed'}
                 </div>
             </div>
